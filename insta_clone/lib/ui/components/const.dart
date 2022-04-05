68f205a0dfdd0b6b06a0c0d5a6eb1875389a739c
BinLucky <87031880+BinLucky@users.noreply.github.com>
2022-04-05 13:27:38 +0300

    Icon Updates

--- insta_clone/lib/ui/components/const.dart
+++ insta_clone/lib/ui/components/const.dart
@@ -6,6 +6,7 @@ const String navbariconPath = "assets/icons/bottom_nav_bar/";
 final Widget reelsIcon = SvgPicture.asset(navbariconPath + "reels.svg");
 final Widget profileIcon = SvgPicture.asset(navbariconPath + "profile.svg");
 final Widget shoppingIcon = SvgPicture.asset(navbariconPath + "shopping.svg");
+
 //final Widget profileIcon = SvgPicture.asset(navbariconPath + "profile.svg");
 
 List<BottomNavigationBarItem> NavList = [
