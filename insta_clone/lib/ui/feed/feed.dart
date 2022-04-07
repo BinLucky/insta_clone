bf49daef0169d00b859641cb63e66cf753204f58
BinLucky <yavuztrx@gmail.com>
2022-04-08 01:10:17 +0300

    Assets

--- insta_clone/lib/ui/feed/feed.dart
+++ insta_clone/lib/ui/feed/feed.dart
@@ -19,13 +19,37 @@ class FeedBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return CustomScrollView(scrollDirection: Axis.horizontal, slivers: [
-      SliverList(
-        delegate:
-            SliverChildBuilderDelegate((BuildContext context, int itemCount) {
-          return CircleAvatar();
-        }, childCount: 10),
+    return Container(
+      alignment: Alignment.topLeft,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 75,
+            child: Stories(),
+            width: 400,
+          ),
+        ],
       ),
-    ]);
+    );
+  }
+}
+
+class Stories extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int i) {
+          return CircleAvatar();
+        },
+        separatorBuilder: (BuildContext context, int i) {
+          return const SizedBox(
+            height: 5,
+            width: 5,
+          );
+        },
+        itemCount: 5);
   }
 }
